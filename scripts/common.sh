29630f0223f31d60d28ebf1881ee401d30b7a4fd
jorgen <jorgenlou@163.com>
2025-07-03 13:26:42 +0800

    refactor(common): 优化代码结构，减少重复逻辑

--- scripts/common.sh
+++ scripts/common.sh
@@ -7,26 +7,18 @@ load_os_release() {
             source /etc/os-release
             __OS_RELEASE_LOADED=1
         else
-            echo "⚠️ 无法加载 /etc/os-release"
+            echo "⚠️ 无法加载文件 /etc/os-release，将进入手动设置模式"
         fi
     fi
 }
 
 get_os_info() {
-    echo "🔎 开始检测系统版本信息"
-    load_os_release
-    if [ -n "$ID" ]; then
-        OS_NAME=$ID
-        OS_VERSION=$VERSION_ID
-        OS_CODENAME=$VERSION_CODENAME
-        export OS_NAME OS_VERSION OS_CODENAME
-        echo "🧭 自动检测系统成功，发行版本：$OS_NAME ($OS_VERSION / $OS_CODENAME)"
-    else
-        echo "⚠️ 无法自动识别系统信息"
-        manual_os_name
-        manual_os_version
-        manual_os_codename
-    fi
+    echo ""
+    echo "🔎 检测系统版本信息"
+    manual_os_name "no echo"
+    manual_os_version "no echo"
+    manual_os_codename "no echo"
+    echo "🧭 检测成功，系统版本为：$OS_NAME-$OS_VERSION ($OS_CODENAME)"
 }
 
 
@@ -34,10 +26,9 @@ manual_os_name() {
     load_os_release
     if [ -n "$ID" ]; then
         OS_NAME=$ID
-        echo "🧭 自动检测系统成功，发行版本：$OS_NAME"
     else
-        echo "⚠️ 系统类型识别失败，请手动选择您的 Linux 系统类型（输入数字选择或 e 退出）："
-        echo "1:Ubuntu 2:Debian 3:CentOS 4:RedHat e:退出"
+        echo "⚠️ 未检测到系统发行版本，请手动选择（输入数字选择或 q 退出）："
+        echo "1:Ubuntu 2:Debian 3:CentOS 4:RedHat q:退出"
         while true; do
             read -p "你的选择: " choice
             case "$choice" in
@@ -45,11 +36,13 @@ manual_os_name() {
                 2) OS_NAME="debian"; break;;
                 3) OS_NAME="centos"; break;;
                 4) OS_NAME="redhat"; break;;
-                e|E) echo "👋 退出"; exit 0;;
-                *) echo "❌ 无效选择，请输入 1/2/3/4/e";;
+                q|Q) echo "👋 退出"; exit 0;;
+                *) echo "❌ 无效选择，请输入 1/2/3/4/q";;
             esac
         done
-        echo "✅ 手动选择系统：$OS_NAME"
+    fi
+    if [ -z "$1" ]; then
+        echo "📌 发行版本为：$OS_NAME"
     fi
     export OS_NAME
 }
@@ -58,9 +51,11 @@ manual_os_version() {
     load_os_release
     if [ -n "$VERSION_ID" ]; then
         OS_VERSION=$VERSION_ID
-        echo "🧭 自动检测系统版本成功：$OS_VERSION"
     else
         read -p "版本号检测失败请输入: " OS_VERSION
+    fi
+    if [ -z "$1" ]; then
+        echo "📌 版本号为：$OS_VERSION"
     fi
     export OS_VERSION
 }
@@ -69,9 +64,11 @@ manual_os_codename() {
     load_os_release
     if [ -n "$VERSION_CODENAME" ]; then
         OS_CODENAME=$VERSION_CODENAME
-        echo "🧭 自动检测系统代号成功：$OS_CODENAME"
     else
         read -p "版本代号检测失败请输入: " OS_CODENAME
+    fi
+    if [ -z "$1" ]; then
+        echo "📌 版本代号为：$OS_CODENAME"
     fi
     export OS_CODENAME
 }

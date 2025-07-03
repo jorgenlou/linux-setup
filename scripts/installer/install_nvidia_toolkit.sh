17b83ce80813f8c506e439866dbd32b8c97ae485
jorgen <jorgenlou@163.com>
2025-07-03 21:08:47 +0800

    refactor(appinstall): 安装脚本添加同一入口

--- scripts/installer/install_nvidia_toolkit.sh
+++ scripts/installer/install_nvidia_toolkit.sh
@@ -31,3 +31,18 @@ dnf_install(){
         libnvidia-container-tools-${TOOLKIT_VERSION} \
         libnvidia-container1-${TOOLKIT_VERSION}
 }
+
+do_install() {
+    case "$OS_NAME" in
+        ubuntu|debian)
+            apt_install
+            ;;
+        rhel|centos|fedora|amzn)  # amzn 是 Amazon Linux 的 /etc/os-release 中的 ID
+            dnf_install
+            ;;
+        *)
+            echo "❌ 不支持的系统类型：$OS_NAME"
+            return 1
+            ;;
+    esac
+}
